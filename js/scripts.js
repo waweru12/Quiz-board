ab5ce0fa0c02f9a53ea1a8255c198ed8051c9fac
Santa Claus <santa@northpole.com>
2019-04-01 11:35:02 +0300

    html inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,29 @@
-// IDEA: backend
-var om = parseInt(qn1+qn2+qn3+qn4+qn5)
+function add(question1, question2, question3, question4, question5) {
+  return question1 + question2 + question3 + question4 + question5;
+};
+
+
+
+$(document).ready(function () {
+  $("#quiz").submit(function (event) {
+    event.preventDefault();
+    var question1 = parseInt($('input:radio[name=answer1]:checked').val());
+    var question2 = parseInt($('input:radio[name=answer2]:checked').val());
+    var question3 = parseInt($('input:radio[name=answer3]:checked').val());
+    var question4 = parseInt($('input:radio[name=answer4]:checked').val());
+    var question5 = parseInt($('input:radio[name=answer5]:checked').val());
+    var result = add(question1, question2, question3, question4, question5);
+
+    $("#sequel").text("YOUR SCORE: " + result + " POINTS OUT OF 50");
+
+    if (sequel >= 40) {
+      $("#comment").text("EXCELLENT WORK! KEEP IT UP.")
+    } else if (result >= 25 && result < 40) {
+      $("#comment").text("FAIRLY DONE! GOOD WORK.")
+    } else if (result < 25) {
+      $("#comment").text("POOR WORK! RETAKE THE TEST.")
+    } else {
+      $("#comment").text("ANSWER EVERY QUESTION PLEASE!")
+    }
+  });
+});
